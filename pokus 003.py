363218e48edec07c64f78b7b6ae9c2426ccde732
zuzanadivinova <zuzana.divinova@gmail.com>
2025-04-17 17:44:37 +0200

    nove kody

--- pokus 003.py
+++ pokus 003.py
@@ -1,2 +1,9 @@
-print ("treti soubor")
-print
+cislo = int(input("Zadej celé číslo:"))
+if cislo % 3 == 0 and cislo % 5 == 0:
+    print("FIZZBUZZ")
+elif cislo % 3 == 0 :
+    print("FIZZ")
+elif cislo % 5 == 0 :
+    print("BUZZ")
+else:
+    print(cislo)
--- pokus002.py
+++ pokus002.py
@@ -1,5 +1,27 @@
-
-print ("ahoj")
-print
-print
-print ("novy radek")
+slovnik = {
+    "kočka": "cat",
+    "pes": "dog",
+    "strom": "tree",
+    "auto": "car",
+    "dům": "house"
+}
+pocet_slov=len(slovnik)
+print(f"Slovník obsahuje: { pocet_slov} slov!")
+for klic in slovnik.keys() :
+    print(klic)
+for hodnota in slovnik.values() :
+    print(hodnota)
+novy_klic=input("Zadej nové slovo: ")
+novy_preklad=input("Zadej nový překlad: ")
+slovnik[novy_klic] = novy_preklad
+print("Nový slovník:", slovnik)
+slovo_uzivatele=input("Zadej slovo: ")
+if slovo_uzivatele in slovnik:
+    print(f"Překlad slova '{slovo_uzivatele}' je: {slovnik[slovo_uzivatele]}")
+else:
+    print(f"Slovo '{slovo_uzivatele}' není ve slovníku.")
+slovo_vymaz=input("Zadej slovo na smazání: ")
+if slovo_vymaz in slovnik :
+    slovnik.pop(slovo_vymaz)
+else:
+    print("Slovo není ve slovníku:")
